7c3a2b90b27658e012884c3b62aba22bb8fd3c37
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-04-23 10:06:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function logWhisper(string) {
   console.log(string)
 }
 function sayHiToGrandma (string) {
-  if (string.toLowerCase() === string)
-  return "I can't hear you!"
-  if (string.toUpperCase() === string)
-  return "YES INDEED!"
-  if (string === "I love you Grandma")
-  return "I love you too"
+  {if (string.toLowerCase() === string)
+  return "I can't hear you!"}
+  {if (string.toUpperCase() === string)
+  return "YES INDEED!"}
+  {if (string === "I love you Grandma")
+  return "I love you too"}
 }
 
   
