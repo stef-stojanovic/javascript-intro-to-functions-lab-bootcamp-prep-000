a93ee21a639cda1960f45a8a1a13c504e2c80ab5
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-04-23 10:01:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+  return string.toUpperCase() 
+}
+function whisper(string) {
+  return string.toLowerCase() 
+}
+
+function logShout(string) {
+  var upperCase = string.toUpperCase()
+  string = upperCase
+  console.log(string)
+}
+function logWhisper(string) {
+  var lowerCase = string.toLowerCase()
+  string = lowerCase
+  console.log(string)
+}
+function sayHiToGrandma ()
+if (string.toLowerCase() === string)
+  return "I can't hear you"
